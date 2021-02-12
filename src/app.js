1362334ee9fa02f0122a79b8fdd5db9be6d8413d
J Bharat <M1053914@mindtree.com>
2021-02-12 16:22:54 +0530

    module 3

--- src/app.js
+++ src/app.js
@@ -1,20 +1,17 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
+const accounts = JSON.parse(accountData)
+const { accounts, users, writeJSON } = require('./data')
 
-const app = express()
+ const app = express()
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
-
-const accountData = fs.readFileSync('src/json/accounts.json', 'UTF8' )
-const accounts = JSON.parse(accountData)
-
-const userData = fs.readFileSync('src/json/users.json', 'UTF8' )
-const users = JSON.parse(userData)
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Account Summary', accounts: accounts})
@@ -49,8 +46,7 @@ app.post('/transfer', (req, res) => {
     accounts[from].balance = from_bal - parseInt(amount)
     accounts[to].balance = to_bal + parseInt(amount)
 
-    let accountsJSON = JSON.stringify(accounts)
-    fs.writeFileSync(path.join(__dirname, '/json','accounts.json'), accountsJSON, 'utf8')
+    writeJSON()
 
     res.render('transfer', {message: 'Transfer Completed'})
 })
@@ -63,8 +59,7 @@ app.post('/payment', (req, res) => {
     accounts.credit.balance = accounts.credit.balance - req.body.amount
     accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount)
     
-    let accountsJSON = JSON.stringify(accounts)
-    fs.writeFileSync(path.join(__dirname, '/json','accounts.json'), accountsJSON, 'utf8')
+    writeJSON()
 
     res.render('payment', {message: 'Payment Successful', account: accounts.credit})
 })
