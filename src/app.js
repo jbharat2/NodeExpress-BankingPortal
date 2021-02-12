213c53257c11fd3f8b0a871bbc3318019c04da77
J Bharat <M1053914@mindtree.com>
2021-02-12 15:43:15 +0530

    Module 2 initial push

--- src/app.js
+++ src/app.js
@@ -9,8 +9,30 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', UTF8 )
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync('src/json/users.json', UTF8 )
+const users = JSON.parse(userData)
+
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'})
+    res.render('index', {title: 'Account Summary', accounts: accounts})
+})
+
+app.get('/savings', (req, res) => {
+    res.render('account', {accounts: accounts.savings})
+})
+
+app.get('/checking', (req, res) => {
+    res.render('account', {accounts: accounts.checking})
+})
+
+app.get('/credit', (req, res) => {
+    res.render('account', {accounts: accounts.credit})
+})
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {user: users[0]})
 })
 
 app.listen(3000, () => {
